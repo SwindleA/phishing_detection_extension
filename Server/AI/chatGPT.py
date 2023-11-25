7bd2b324191a26b74b046c43446a172e8f9ed045
Adrian Swindle <aswindle724@gmail.com>
2023-11-24 21:07:36 -0600

    added template for asking gpt for reevaluation

--- Server/AI/chatGPT.py
+++ Server/AI/chatGPT.py
@@ -38,6 +38,31 @@ class chatGPT:
         return response['choices'][0]['message']['content']
 
 
+    def reEvaluation(question):
+
+        while True:
+
+            try:
+
+                response = openai.ChatCompletion.create(
+                    model="gpt-3.5-turbo",
+                    messages=[{"role": "user", "content" : question}],
+                    temperature=0.7,
+                    max_tokens=2048,
+                    top_p=1.0,
+                    frequency_penalty=0.0,
+                    presence_penalty=0.0
+                )
+                break
+            except openai.error.ServiceUnavailableError as e:
+                print("API error:", e)
+                print("Retrying in 10 seconds...")
+                time.sleep(10)
+            except Exception as ex:
+                print("API exception:", ex)
+                print("Retrying in 10 seconds...")
+                time.sleep(10)
+
 ## for testing
 
 #print(askQuestion("How much do blue whales weigh?"))
