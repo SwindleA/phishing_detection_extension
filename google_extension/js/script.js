2b682544668007cec9d50c9e8134c0c7cada13df
Adrian Swindle <aswindle724@gmail.com>
2023-11-24 19:50:34 -0600

    Fix retreiving email message and checklist draft
    It was discovered that the entire email message was not being pulled.
    This was fix. Two things: emails can be too long, and not all the email
    contents can be retrieved.
    
    A draft for the checklist aspect has be created.

--- google_extension/js/script.js
+++ google_extension/js/script.js
@@ -1,7 +1,7 @@
 import { Request } from "./request.js";
 import { Button } from "./Button.js";
 
-const API_KEY = 
+const API_KEY = 'AIzaSyDGyvU7VWNHH9U-9lhb_GVP2YE-gN1OH4s'
 class App{
 
     constructor(){
@@ -15,7 +15,9 @@ class App{
         this.evaluate_email = this.evaluateEmail();
 
         this.Reevaluate_email = this.ReevaluateEmail();
-        const element = document.getElementById("myButton");
+
+        this.submitChecklist = this.submitChecklist();
+
 
     }
 
@@ -35,13 +37,29 @@ class App{
       }
 
 
-    ReevaluateEmail(){
+      ReevaluateEmail(){
         const button = new Button('#reevaluate', ()=>{
             console.log("ReevaluateButton");
 
+            document.getElementById('main_screen').style.display = "none";
 
+            document.getElementById('checklist').style.display = "block";
         });
-    }
+        button.render();
+       }
+
+        submitChecklist(){
+            const button = new Button('#submit',()=>{
+                let checkboxes = document.querySelectorAll('input:checked');
+
+                let checkedItems = Array.from(checkboxes).map(checkbox => checkbox.name);
+                console.log(checkedItems);
+
+            });
+
+            button.render();
+
+        }
 
 
       evaluateEmail(){
@@ -66,6 +84,22 @@ class App{
 
                       return resultStr;
                   };
+                  //takes the array if parts in as the parameter, this is for extracting the message from an email
+                  function getMessage(part) {
+                      console.log("part: ", part);
+                      let total=''
+                    for(let p=0;p<part.length;p++){
+                        console.log("p: ", part[p]);
+                        if(part[p].mimeType === 'multipart/alternative'){
+                            console.log("here");
+                            total += getMessage(part[p].parts);
+                        }else if(part[p].mimeType === 'text/plain'){
+                            total += part[p].body.data;
+                        }
+                    }
+
+                    return total;
+                  };
 
                   chrome.scripting.executeScript({
                       target: { tabId: tab.id },
@@ -73,13 +107,13 @@ class App{
                       }).then((rep) => {
 
                       const message_id = rep[0]['result'];
-                      console.log("message_id: ", message_id)
+                      // console.log("message_id: ", message_id)
 
                       if(message_id == null){console.error("Message Id not found.");}
                       else {
                           chrome.identity.getAuthToken({interactive: true}, function (token) {
 
-                              console.log("auth token: ", token);
+                              //console.log("auth token: ", token);
                               if(token == null){console.error("Authorization token null");}
                               else{
                                     chrome.identity.getProfileUserInfo(function (accounts) {
@@ -97,8 +131,31 @@ class App{
                                       .then((response) => {
                                           response.json().then((element) => {
 
+                                              //parts are the elements of the readable message, includes images,html stuff, etc...
+                                              console.log(element.payload.parts);
+                                              //get the plaintext parts of the email
+
+                                              let encoded = getMessage(element.payload.parts);
+                                                // base64 does not support '-' and '_'
+                                              // use '-' => '+' and '_'=>'/'
+                                              let encoded2 = encoded.replace('-','+');
+                                              let encoded3 = encoded2.replace('_','/');
+                                              console.log("encoded:  ",encoded3);
+
+                                                //convert from base64 to plaintext
+                                              let decoded = window.atob(encoded3);
+                                              console.log("decoded: ", decoded);
+
                                               const sender_email = element.payload.headers[7]['value'];
                                               console.log(sender_email);
+
+                                              //split the email by the @ sign, then split by the '>' to remove it from the end. Emails here start with '<' and end with '>', removing the arrows for clarity.
+                                              let email_domain = '@'+sender_email.split("@")[1].split('>')[0];
+                                              //add sender email to the checklist
+                                              const checklist_sender = document.getElementById('domain');
+                                                if (checklist_sender){
+                                                    checklist_sender.innerText  = email_domain;
+                                                }
 
                                               const recipient_email = element.payload.headers[0]['value'];
                                               console.log(recipient_email);
@@ -106,14 +163,13 @@ class App{
                                               const subject = element.payload.headers[19]['value'];
                                               console.log(subject);
 
-                                              const email_message = element.snippet;
-                                              console.log(email_message);
+
 
                                               const payload = {
                                                   'sender_email': sender_email,
                                                   'recipient_email': recipient_email,
                                                   'subject': subject,
-                                                  'email_message' : email_message
+                                                  'email_message' : decoded
                                               }
                                               console.log(payload);
 
@@ -142,7 +198,7 @@ class App{
                                                       if (elm2) {
                                                           elm2.innerHTML = element.evaluation;
                                                       }
-
+                                                      document.getElementById('reevaluate').style.display = "block";
 
 
                                                   })
